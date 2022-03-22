a2d6bbb63cc21d4382ab45247df9c8f0708d67a9
shahzaibk23 <shahzaibceo@gmail.com>
2022-03-22 23:24:42 +0500

    orrr lalalal

--- main2.py
+++ main2.py
@@ -13,45 +13,8 @@ if __name__ == '__main__':
 
     @eel.expose
     def getTheFileStrucuture():
-        import os
-        # rootdir = '/home/mordok/mera_project/'
-        rootdir = project_dir
-        strct = {}
-
-        for subdir, dirs, files in os.walk(rootdir):
-            for file in files:
-                fullFile = os.path.join(subdir, file)
-                fullFile = fullFile.replace(rootdir, "")
-                allFolders = fullFile.split("/")
-                if len(list(allFolders)) == 1:
-                    if "." not in strct.keys():
-                        strct["."] = [fullFile]
-                    else:
-                        strct["."].append(fullFile)
-                else:
-                    
-                    lalala = strct
-                    
-                    for i in range(len(allFolders)):
-                        if i+1 == len(allFolders)-1:
-                            if allFolders[i] in lalala.keys():
-                                
-                                lalala[allFolders[i]]["."].append(allFolders[i+1])
-                                break
-                            else:
-                                lalala[allFolders[i]] ={
-                                    ".":[allFolders[i+1]]
-                                } 
-                                break
-                        else:
-                            if allFolders[i] in lalala.keys():
-                                lalala = lalala[allFolders[i]]
-                            else:
-                                lalala[allFolders[i]] = {}
-                                lalala = lalala[allFolders[i]]
-
-        eel.pakrKayLaoFiles(strct)
-        # return getFileStructure(project_dir)
+        structure = getFileStructure(project_dir)
+        eel.pakrKayLaoFiles(structure)
 
     start('index.html', mode='custom', cmdline_args=['node_modules/electron/dist/electron', '.'], port=8008)
 
