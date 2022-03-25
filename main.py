e1cd9af1cf50cf9886561ffb1d9b3d28196889b8
Mahnoor-ismail01 <mahnoor@students.uit.edu>
2022-03-25 23:19:13 +0500

    backend connect

--- main.py
+++ main.py
@@ -2,12 +2,17 @@ import eel,os
 from eel import init, start
 from replacer import replacer
 from reverter import reverter
+import os ,random 
+import tkinter 
+import tkinter.filedialog as filedialog
+import shutil
 
 
 if __name__ == '__main__':
 
     
     init('web')
+    list1 =[]  
 
     @eel.expose
     def stop_everything():
@@ -19,8 +24,39 @@ if __name__ == '__main__':
     @eel.expose                         # Expose this function to Javascript
     def say_hello_py(x):
         print('Hello from %s' % x)
+    @eel.expose
+    def socdetail(listsoc):
+        dictsoc={"i": 0, "m": 0, "f": 0, "c": 0, "gpio": 0, "spi": 0, "uart": 0, "timer": 0, "spi_flash": 0, "i2c": 0, "wb": 0, "tl": 0}
+        
+        for val in listsoc:
+            for key in dictsoc:
+                
+                if key==val:
+                    dictsoc[key]=1
+        print(dictsoc)
+        file=open("web/SoC-Now-Generator/src/main/scala/config.json","w")
+        file.write(str(dictsoc))
+        file.close()
+        yourproject=list1[0]
+        os.system("cp -a web/SoC-Now-Generator/src/main/scala yourproject")
+        #shutil.copytree("web/SoC-Now-Generator/src/main/scala", yourproject)
+        eel.goToMain()
+    
+
+    @eel.expose
+
+    def selectFolder():
+        print("Here")
+        root = tkinter.Tk()
+        root.attributes("-topmost", True)
+        root.withdraw()
+        directory_path = filedialog.askdirectory()
+        print(directory_path)
+        list1.append(directory_path)
+        print(list1[0],'o')
+        eel.select_js(list1[0])
 
     reverter()
-    start('splash.html', mode='custom', cmdline_args=['node_modules/electron/dist/electron', '.'], port=8003)
+    start('splash.html', mode='custom', cmdline_args=['node_modules/electron/dist/electron', '.'], port=8013)
 
 
