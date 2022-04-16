1da17a5a89282c03bf44fa8daff81416dad14d09
Mahnoor-ismail01 <mahnoor@students.uit.edu>
2022-04-17 01:33:12 +0500

    connected parser in main

--- main2.py
+++ main2.py
@@ -1,25 +1,33 @@
+import imp
+from multiprocessing.spawn import import_main_path
 import eel, os
 from eel import init, start
-from utils import getFileStructure      # file structure utility
-
+from utils import getFileStructure, parseFileStructure      # file structure utility
+from icecream import ic
 if __name__ == '__main__':
 
 
 
-
-    project_dir = "/home/mordok/mera_project"
+    f = open("web/pathfile","r")
+    c = f.readlines()
+    f.close()
+    project_dir = c[0]  #"/home/mordok/mera_project"
     
     init('web')
 
     @eel.expose
     def getTheFileStrucuture():
         structure = getFileStructure(project_dir)
-        eel.pakrKayLaoFiles(structure)
+        parsedHTML = ""
+        parsedHTML = parseFileStructure(structure[""])
+        # print(parsedHTML)
+        eel.pakrKayLaoFiles(parsedHTML)
     @eel.expose
     def genrtlpy():
         file=open("web/pathfile","r")
         contents= file.readlines()
-        print(contents[0])
+        file.close()
+        ic(contents[0])
         
         os.chdir(f"{contents[0]}/SoC-Now-Generator")
         os.system("sbt 'runMain GeneratorDriver'")
