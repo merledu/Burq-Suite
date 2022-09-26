1222023f622678f4fca7fd856d42a19328ff9d8c
CyberGai <cyb3rgai@proton.me>
2022-09-27 00:41:32 +0500

    updated logCompare

--- scripts/logCompare.py
+++ scripts/logCompare.py
@@ -15,7 +15,14 @@ class LogComparator:
     type10 = ('mv')
     type11 = ('nop')
 
-    rvfiParams = ('insn', 'mode', 'rs1_addr', 'rs2_addr', 'rd_addr', 'rd_wdata', 'mem_addr', 'mem_rdata', 'mem_wdata')
+    gpr = [
+        'zero', 'ra', 'sp',  'gp',  'tp', 't0', 't1', 't2',
+        's0',   's1', 'a0',  'a1',  'a2', 'a3', 'a4', 'a5',
+        'a6',   'a7', 's2',  's3',  's4', 's5', 's6', 's7',
+        's8',   's9', 's10', 's11', 't3', 't4', 't5', 't6'
+    ]
+
+    rvfiParams = ('insn', 'rs1_addr', 'rs2_addr', 'rd_addr', 'mem_addr', 'mem_rdata', 'mem_wdata')
 
     def __init__(self):
         self.rvfiDict = {
@@ -28,7 +35,7 @@ class LogComparator:
             #'pc_rdata' : [],  # pc_rdata excluded due to mismatch from Spike log
             #'pc_wdata' : [],  # pc_wdata excluded due to mismatch from Spike log
             'insn'     : [],
-            'mode'     : [],
+            #'mode'     : [],
             'mem_addr' : [],
             'mem_rdata': [],
             'mem_wdata': []
@@ -36,30 +43,27 @@ class LogComparator:
         self.logList = []
 
     def spikeLogExtract(self, filename, debug=False):
-        # File should be a .log file
-        assert filename[-4:] == '.log',\
-            "File is not a .log file"
-
         # Reading from file
         with open(filename, 'r', encoding='UTF-8') as f:
             logList = f.readlines()
 
-        # Converting the two line instructions into a single element list
+        # Refining the log
         logList = [logList[i] for i in range(11, len(logList))]
         logList = [
-            sub('\n', '', f'{logList[i][logList[i].find(":") + 2:]} {logList[i + 1][logList[i + 1].find(":") + 2:]}') for i in range(0, len(logList) - 1, 2)
+            sub(
+                '\n', '',
+                f'{logList[i][logList[i].find(":") + 2:]} {logList[i + 1][logList[i + 1].find(":") + 2:]}'
+            ) for i in range(0, len(logList) - 1, 2)
         ]
         self.logList = [split('\s+', _) for _ in logList]
         self.logList = [_ for _ in self.logList if _[0] != 'exception']
-
-        #for _ in self.logList: print(_)
 
         # Extracting values
         for i in range(len(self.logList)):
             entry = self.logList[i]
 
             self.rvfiDict['insn'].append(entry[1][3: -1])
-            self.rvfiDict['mode'].append('3')
+            #self.rvfiDict['mode'].append('3')
 
             # type2
             if entry[2] in LogComparator.type2:
@@ -208,34 +212,23 @@ class LogComparator:
                 self.rvfiDict['mem_rdata'].append('00000000')
                 self.rvfiDict['mem_wdata'].append('00000000')
 
-        #print(self.rvfiDict)
-        if debug == True:
-            with open('./spikeTest.txt', 'w', encoding='UTF-8') as f:
-                for k in self.rvfiDict:
-                    for i in range(len(k)):
-                        f.write(f'{k}:')
-                        f.writelines(self.rvfiDict[k])
-                        f.write('\n')
+        return
 
     def coreLogExtract(self, filename):
-        # File should be a .csv file
-        assert filename[-4:] == '.csv',\
-            "File is not a .csv file"
-
         # Reading from file
         with open(filename, 'r', encoding='UTF-8') as f:
             logList = f.readlines()
 
         # Refining the log
         self.logList = [split(',', sub('\n', '', _)) for _ in logList]
-        for _ in self.logList:print(_)
+        #for _ in self.logList: print(_)
 
         # Extracting values
         for i in range(1, len(self.logList)):
             entry = self.logList[i]
         
             self.rvfiDict['insn'].append(entry[-5])
-            self.rvfiDict['mode'].append(entry[-4])
+            #self.rvfiDict['mode'].append(entry[-4])
 
             self.rvfiDict['rs1_addr'].append(entry[2])
             self.rvfiDict['rs2_addr'].append(entry[4])
@@ -247,15 +240,156 @@ class LogComparator:
             self.rvfiDict['mem_rdata'].append(entry[-2])
             self.rvfiDict['mem_wdata'].append(entry[-1])
 
-        #for k in self.rvfiDict:
-        #    print(f'{k}:')
-        #    for _ in self.rvfiDict[k]: print(_)
-        #    print('')
+        return
 
-    def match(self, other, debug = False):
+    def ibexLogExtract(self, filename):
+        # Reading from file
+        with open(filename, 'r', encoding='UTF-8') as f:
+            logList = f.readlines()
+
+        # Refining the log
+        self.logList = [split('\s+', sub('\n', '', _)) for _ in logList]
+        for i in range(1, len(self.logList)):
+            self.logList[i][6] = self.logList[i][6].split(',')
+
+        # Extracting values
+        for i in range(1, len(self.logList)):
+            entry = self.logList[i]
+
+            self.rvfiDict['insn'].append(entry[4])
+
+            # type 1
+            if entry[5] in LogComparator.type1:
+                self.rvfiDict['rs1_addr'].append(LogComparator.gpr[int(entry[6][1][1:])])
+                self.rvfiDict['rs2_addr'].append(LogComparator.gpr[0])
+
+                self.rvfiDict['rd_addr'].append(LogComparator.gpr[int(entry[6][0][1:])])
+                #self.rvfiDict['rd_wdata'].append(entry[-1][entry[-1].find('0x') + 2:])
+
+                self.rvfiDict['mem_addr'].append('00000000')
+                self.rvfiDict['mem_rdata'].append('00000000')
+                self.rvfiDict['mem_wdata'].append('00000000')
+            
+            # type 2
+            elif entry[5] in LogComparator.type2:
+                self.rvfiDict['rs1_addr'].append(LogComparator.gpr[0])
+                self.rvfiDict['rs2_addr'].append(LogComparator.gpr[0])
+
+                self.rvfiDict['rd_addr'].append(LogComparator.gpr[int(entry[6][0][1:])])
+                #self.rvfiDict['rd_wdata'].append()
+
+                self.rvfiDict['mem_addr'].append('00000000')
+                self.rvfiDict['mem_rdata'].append('00000000')
+                self.rvfiDict['mem_wdata'].append('00000000')
+
+            # type 3
+            elif entry[5] in LogComparator.type3:
+                self.rvfiDict['rs1_addr'].append(LogComparator.gpr[0])
+                self.rvfiDict['rs2_addr'].append(LogComparator.gpr[0])
+
+                self.rvfiDict['rd_addr'].append(LogComparator.gpr[int(entry[6][0][1:])])
+                #self.rvfiDict['rd_wdata'].append()
+
+                self.rvfiDict['mem_addr'].append('00000000')
+                self.rvfiDict['mem_rdata'].append('00000000')
+                self.rvfiDict['mem_wdata'].append('00000000')
+
+            # type 4
+            elif entry[5] in LogComparator.type4:
+                self.rvfiDict['rs1_addr'].append(LogComparator.gpr[int(entry[6][1][int(entry[6][1].find('(') + 2): -1])])
+                self.rvfiDict['rs2_addr'].append(LogComparator.gpr[int(entry[6][0][1:])])
+
+                self.rvfiDict['rd_addr'].append(LogComparator.gpr[0])
+                #self.rvfiDict['rd_wdata'].append()
+
+                self.rvfiDict['mem_addr'].append(entry[-3][entry[-3].find('0x') + 2:])
+                self.rvfiDict['mem_rdata'].append('00000000')
+                self.rvfiDict['mem_wdata'].append(entry[-2][entry[-2].find('0x') + 2:])
+
+            # type 5
+            elif entry[5] in LogComparator.type5:
+                self.rvfiDict['rs1_addr'].append(LogComparator.gpr[int(entry[6][1][int(entry[6][1].find('(') + 2): -1])])
+                self.rvfiDict['rs2_addr'].append(LogComparator.gpr[0])
+
+                self.rvfiDict['rd_addr'].append(LogComparator.gpr[int(entry[6][0][1:])])
+                #self.rvfiDict['rd_wdata'].append()
+
+                self.rvfiDict['mem_addr'].append(entry[-2][entry[-2].find('0x') + 2:])
+                self.rvfiDict['mem_rdata'].append('00000000')
+                self.rvfiDict['mem_wdata'].append(entry[-3][entry[-3].find('0x') + 2:])
+
+            # type 6
+            elif entry[5] in LogComparator.type6:
+                self.rvfiDict['rs1_addr'].append(LogComparator.gpr[int(entry[6][0][1:])])
+                self.rvfiDict['rs2_addr'].append(LogComparator.gpr[int(entry[6][1][1:])])
+
+                self.rvfiDict['rd_addr'].append(LogComparator.gpr[0])
+                #self.rvfiDict['rd_wdata'].append()
+
+                self.rvfiDict['mem_addr'].append('00000000')
+                self.rvfiDict['mem_rdata'].append('00000000')
+                self.rvfiDict['mem_wdata'].append('00000000')
+
+            # type 7
+            elif entry[5] in LogComparator.type7:
+                self.rvfiDict['rs1_addr'].append(LogComparator.gpr[int(entry[6][1][1:])])
+                self.rvfiDict['rs2_addr'].append(LogComparator.gpr[int(entry[6][2][1:])])
+
+                self.rvfiDict['rd_addr'].append(LogComparator.gpr[int(entry[6][0][1:])])
+                #self.rvfiDict['rd_wdata'].append()
+
+                self.rvfiDict['mem_addr'].append('00000000')
+                self.rvfiDict['mem_rdata'].append('00000000')
+                self.rvfiDict['mem_wdata'].append('00000000')
+
+            # type 8
+            elif entry[5] in LogComparator.type8:
+                self.rvfiDict['rs1_addr'].append(LogComparator.gpr[int(entry[6][1][entry[6][1].find('(') + 2: -1])])
+                self.rvfiDict['rs2_addr'].append(LogComparator.gpr[0])
+                
+                self.rvfiDict['rd_addr'].append(LogComparator.gpr[int(entry[-1][entry[-1].find('0x') + 2:])])
+                #self.rvfiDict['rd_wdata'].append()
+
+                self.rvfiDict['mem_addr'].append('00000000')
+                self.rvfiDict['mem_rdata'].append('00000000')
+                self.rvfiDict['mem_wdata'].append('00000000')
+
+        return
+
+    def match(self, other, debug=False, quickMatch=False):
         # Check to see if the program length is the same
-        assert len(self.rvfiDict['insn']) == len(other.rvfiDict['insn']),\
-            "Core output did not match"
+        if quickMatch == True:
+            assert len(self.rvfiDict['insn']) == len(other.rvfiDict['insn']),\
+                "Core output did not match"
+
+        #if debug == True:
+        #    with open('compare.log', 'w', encoding='UTF-8') as f:
+        #        f.write(f'{0:15}{1}\n\n'.format('Spike', 'Core'))
+
+        #        for k in LogComparator.rvfiParams:
+        #            f.write('{0}\n'.format(k))
+        #            print(len(self.rvfiDict[k]))
+        #            print(len(other.rvfiDict[k]))
+
+        #            if len(self.rvfiDict[k]) >= len(other.rvfiDict[k]):
+        #                maxInstLen = len(self.rvfiDict[k])
+        #                minInstLen = len(other.rvfiDict[k])
+
+        #            else:
+        #                maxInstLen = len(other.rvfiDict[k])
+        #                minInstLen = len(self.rvfiDict[k])
+
+        #            for i in range(minInstLen):
+        #                f.write('{0:15}{1}\n'.format(k[i], other.rvfiDict[k][i]))
+
+        #            for i in range(minInstLen, minInstLen + (maxInstLen - minInstLen)):
+        #                if len(self.rvfiDict[k]) >= len(other.rvfiDict[k]):
+        #                    f.write(f'{0:15}\n'.format(k[i]))
+
+        #                else:
+        #                    f.write(f'{" " * 15}{0}\n'.format(other.rvfiDict[k][i]))
+        #            
+        #            f.write('\n')
 
         # Iterate through each RVFI parameter
         for _ in self.rvfiDict:
@@ -275,58 +409,47 @@ class LogComparator:
         else:
             return True
 
-    def debug(self, other, enable=True):
-        if enable != True:
-            return
+    def debug(self, core):
+        # Dumping Spike test log
+        with open('spikeTest.log', 'w', encoding='UTF-8') as f:
+            f.write(f'Spike\n\n')
 
-        print('Spike:')
-        for i in range(len(self.rvfiDict['insn'])):
-            print(f"line {i}: {self.rvfiDict['insn'][i]}")
+            for k in LogComparator.rvfiParams:
+                f.write('{0}\n'.format(k))
 
-        print('\nCore:')
-        for i in range(len(other.rvfiDict['insn'])):
-            print(f"line {i}: {self.rvfiDict['insn'][i]}")
+                for i in range(len(self.rvfiDict[k])):
+                    f.write('{0}: {1}\n'.format(i + 1, self.rvfiDict[k][i]))
 
-    def __len__(self):
-        for i in range(1, len(LogComparator.rvfiParams)):
-            if len(self.rvfiDict[LogComparator.rvfiParams[0]]) != len(self.rvfiDict[LogComparator.rvfiParams[i]]):
-                print('Values not extracted correctly')
-                return None
-        return len(self.rvfiDict['insn'])
+                f.write('\n')
 
-    def debug2(self, other):
-        limit = 0
-        print(f'Spike\tCore\n')
-        for k in self.rvfiDict:
-            print(f'{k}\t {k}\n')
-            for i in range(len(self.rvfiDict['insn'])):
-                print(f'{self.rvfiDict[k][i]}\t{other.rvfiDict[k][i]}')
-            else:
-                print('\n')
-            limit += 1
-            if limit == 5: return
-        
-    def __del__(self):
-        pass
+        # Dumping core test log
+        with open('coreTest.log', 'w', encoding='UTF-8') as f:
+            f.write(f'Core\n\n')
 
-    #def tempDebug(self, other):
-    #    print(self.rvfiDict['rd_wdata'][7])
-    #    print(other.rvfiDict['rd_wdata'][7])
+            for k in LogComparator.rvfiParams:
+                f.write('{0}\n'.format(k))
+
+                for i in range(len(core.rvfiDict[k])):
+                    f.write('{0}: {1}\n'.format(i + 1, core.rvfiDict[k][i]))
+
+                f.write('\n')
+
+        return
 
 if __name__ == '__main__':
     spike = LogComparator()
     core  = LogComparator()
+    #ibex  = LogComparator()
 
-    #spike.spikeLogExtract('./Test.log')
+    #spike.spikeLogExtract('./logs/simple_system_cosim.log')
+    spike.spikeLogExtract('./fibonacci.log')
     core.coreLogExtract('./trace.csv')
+    #ibex.ibexLogExtract('./logs/trace_core_00000000.log')
+    #ibex.ibexLogExtract('../6/ibex.log')
 
-    #print(spike.rvfiDict['insn'])
-    #print(core.rvfiDict['insn'])
+    if spike.match(core, debug=True):
+         print('\nCore output matched successfully')
+    else:
+         print('\nCore output match failed')
 
-    #spike.debug(core)
-    #spike.debug2(core)
-
-    #if spike.match(core, debug=True):
-    #     print('\nCore output matched successfully')
-    #else:
-    #     print('\nCore output match failed')
+    #spike.debug(ibex)
