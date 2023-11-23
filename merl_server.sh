03efcfa60c20e50a0a31ef7d6b29e79100b73222
shahzaibk23 <shahzaibceo@gmail.com>
2023-11-24 01:09:00 +0500

    server env vars up

---
+++ merl_server.sh
@@ -0,0 +1,4 @@
+export SPIKE_PATH=/merledu1/tools/riscv64/riscv/bin/bin
+export RISCV_OBJCOPY=/merledu1/tools/riscv64/riscv/bin/riscv64-unknown-elf-objcopy
+export RISCV_GCC=/merledu1/tools/riscv64/riscv/bin/riscv64-unknown-elf-gcc
+export PATH=/merledu1/tools/riscv64/riscv/bin/:$PATH
