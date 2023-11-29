b11f46ca8577f4f9140de1cc6e9f5a0256ea6078
5hayanB <baig.sh@tutanota.com>
2023-11-29 19:59:04 +0500

    temp commit

--- main.py
+++ main.py
@@ -1,29 +1,28 @@
 import os, logging
 
 from importlib import import_module
-from types import ModuleType
 
-riscv_dv: ModuleType = import_module('riscv-dv.riscv_dv_interface')
+riscv_dv = import_module('riscv-dv.riscv_dv_interface')
 
-BURQ_SUITE_ROOT: str = os.path.dirname(os.path.abspath(__file__))
-BURQ_SUITE_LOGS: str = os.path.join(BURQ_SUITE_ROOT, 'logs')
-RISCV_DV_ENV   : str = os.path.join(BURQ_SUITE_ROOT, 'riscv-dv')
-RISCV_DV_ROOT  : str = os.path.join(RISCV_DV_ENV, 'riscv-dv')
+BURQ_SUITE_ROOT = os.path.dirname(os.path.abspath(__file__))
+BURQ_SUITE_LOGS = os.path.join(BURQ_SUITE_ROOT, 'logs')
+RISCV_DV_ENV    = os.path.join(BURQ_SUITE_ROOT, 'riscv-dv')
+RISCV_DV_ROOT   = os.path.join(RISCV_DV_ENV, 'riscv-dv')
 
 
-def main() -> None:
+def main():
     logging.basicConfig(
-        filename = os.path.join(BURQ_SUITE_LOGS, 'a.log'),
-        filemode = 'w',
-        format   = '%(asctime)s %(levelname)-8s %(filename)s %(message)s',
-        datefmt  = '%d %b %Y %H:%M:%S',
-        level    = logging.DEBUG
+        filename=os.path.join(BURQ_SUITE_LOGS, 'a.log'),
+        filemode='w',
+        format  ='%(asctime)s %(levelname)-8s %(filename)s %(message)s',
+        datefmt ='%d %b %Y %H:%M:%S',
+        level   =logging.DEBUG
     )
 
-    riscv_dv.run_test(proj_path = '/home/5hayanB/temporary/riscv-dv-out/1',
-                      noclean   = False,
-                      target    = 'rv32i',
-                      test      = 'riscv_arithmetic_basic_test')
+    riscv_dv.run_test(proj_path='/home/shayan/temporary/riscv-dv-out/1',
+                      noclean  =False,
+                      target   ='rv32i',
+                      test     ='riscv_arithmetic_basic_test')
 
     return None
 
