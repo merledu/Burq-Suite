a15e998264d62aff33e16f42345b29b93be2f43c
5hayanB <baig.sh@tutanota.com>
2024-07-12 18:44:53 +0500

    does not work on mint. Working on arch and ubuntu

--- scripts/utils.py
+++ scripts/utils.py
@@ -20,7 +20,6 @@ def run_cmd(cmd, redirect_to_file=False, stdout_f=''):
         with open(stdout_f, 'w', encoding='UTF-8') as f:
             cmd_run = subprocess.run(cmd, text=True, stdout=f)
     else:
-        print(cmd)
         cmd_run = subprocess.run(cmd, capture_output=True, text=True)
     if cmd_run.returncode:
         logging.info(cmd_run.returncode)
@@ -47,7 +46,6 @@ def gen_disasm(obj_path, dump_path):
 def dut_run_test(obj_path, disasm_dump_path):
     gen_disasm(obj_path, disasm_dump_path)
     os.chdir(configs['dut_path'])
-    print(os.getcwd())
     run_cmd(configs['dut_cmd'].split())
 
 
